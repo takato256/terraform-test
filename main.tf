18ec414c7acac122bdf8840009127be47374eaf9
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2024-01-03 17:14:11 +0900

    [追加] コメントを追加

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
+# プロバイダとしてAWSを使用し、us-east-2リージョンにインフラをデプロイ
 provider "aws" {
   region = "us-east-2"
 }
 
 resource "aws_launch_configuration" "example" {
+  # EC2インスタンスを動かすためのAmazon Machine Image(AMI)のIDを指定
   image_id = "ami-0fb653ca2d3203ac1"
+  # 動かすEC2インスタンスのタイプ
+  # t2.microは1つの仮想CPUと1GBのメモリを持つ
   instance_type = "t2.micro"
+  # セキュリティグループのIDを渡す
   security_groups = [aws_security_group.instance.id]
 
+  # <<-EOFとEOFによって、\nを入れなくても複数行にわたる文字列を渡せる
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello World" > index.html
@@ -60,6 +66,7 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_lb_target_group" "asg" {
   name = "terraform-asg-example"
+  # 変数参照
   port = var.server_port
   protocol = "HTTP"
   vpc_id = data.aws_vpc.default.id
@@ -91,9 +98,12 @@ resource "aws_lb_listener_rule" "asg" {
   }
 }
 
+# AWSはEC2インスタンスから外向き、内向きのトラフィックを許可していないため
+# セキュリティグループを作成する必要がある
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
+  # 0.0.0.0/0のCIDRブロックからポート8080に対する内向きのTCPリクエストを許可
   ingress {
     from_port = 8080
     to_port = 8080
